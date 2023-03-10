05d174fc3d8c50aa5742a6dea9636b29fa9d7b3d
BuenoIT <bueno.itdev@gmail.com>
2023-03-10 17:25:09 -0500

    Initial commit

---
+++ app/src/main/java/com/example/taskmaster/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.taskmaster;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button addButton;
+    private Button viewButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Find the Buttons by their IDs
+        addButton = findViewById(R.id.button_add_task);
+        viewButton = findViewById(R.id.button_view_tasks);
+
+        // Set click listeners for the Buttons
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, AddTasks.class);
+                startActivity(intent);
+            }
+        });
+
+        viewButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, ViewTasks.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/taskmaster/ViewTasks.java
@@ -0,0 +1,16 @@
+package com.example.taskmaster;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ViewTasks extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.view_tasks);
+
+    }
+}
+
